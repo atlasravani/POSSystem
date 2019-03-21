cd055aff428f4917efd45f88f0d242230ccb4ca7
atlasravani <a.sravanireddy59@gmail.com>
2019-03-22 11:32:19 +1300

    Initial commit

---
+++ POSSystem/Calculations.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace POSSystem
+{
+    class Calculations
+    {
+        public static decimal POSCalculation(Dictionary<string, decimal> Products, string selectedProducts)
+        {
+            decimal total = 0.00m;
+            decimal sumA = 0.00m;
+            decimal sumB = 0.00m;
+            decimal sumC = 0.00m;
+            decimal sumD = 0.00m;
+            int offerCountA = 0;
+            var productAcount = Regex.Matches(selectedProducts, "A").Count;
+            var productBcount = Regex.Matches(selectedProducts, "B").Count;
+            var productCcount = Regex.Matches(selectedProducts, "C").Count;
+            var productDcount = Regex.Matches(selectedProducts, "D").Count;
+            
+            if (productAcount >= 3)
+            {
+                offerCountA = productAcount / 3;
+                int remainingA = productAcount - (3 * offerCountA);
+                sumA = (Products["OfferA"] * offerCountA) + (remainingA * Products["A"]);
+            }
+            else
+            {
+                sumA = productAcount * Products["A"];
+            }
+            sumB = productBcount * Products["B"];
+            if (productCcount >= 6)
+            {
+                int offerCountC = productCcount / 6;
+                int remainingC = productCcount - (6 * offerCountC);
+                sumC = (Products["OfferC"] * offerCountC) + (remainingC * Products["C"]);
+
+            }
+            else
+            {
+                sumC = productCcount * Products["C"];
+            }
+            sumD = productDcount * Products["D"];
+            total = sumA + sumB + sumC + sumD;
+            return total;
+        }
+    }
+}
---
+++ POSSystem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dictionary<string, decimal> Products = new Dictionary<string, decimal>();
+            Products.Add("A", 1.25m);
+            Products.Add("B", 4.25m);
+            Products.Add("C", 1.00m);
+            Products.Add("D", 0.75m);
+            Products.Add("OfferA", 3.00m);
+            Products.Add("OfferC", 5.00m);
+            Console.WriteLine("Enter your Products");
+            string productsList = Console.ReadLine().ToString();
+            decimal total = 0.00m;
+            total = Calculations.POSCalculation(Products, productsList);
+            Console.WriteLine("Your Bill for today is $" + total);
+            Console.ReadKey();
+        }
+    }
+}
